ef2fbe85864d2a422c5ed4c6a5462cce22b9ba45
hong <ghhong@wemb.co.kr>
2021-03-11 00:12:39 +0900

    주석 삽입, 샘플데이터 하나 더

--- fluidy-client/src/component/DiagramComponent.tsx
+++ fluidy-client/src/component/DiagramComponent.tsx
@@ -3992,9 +3992,102 @@ const DiagramComponent = (props: any)=>{
               data: { id: 'ab', source: 'a', target: 'b' }
             }
         ]
+        const elements3 = [
+            // Nodes
+            
+            {"data":{"id":"master"}},
+            {"data":{"id":"masterFoo","parent":"master"}},
+            {"data":{"id":"masterComp","parent":"master"}},
+            {"data":{"id":"masterReverb","parent":"master"}},
+            {"data":{"id":"masterLimiter","parent":"master"}},
+            {"data":{"id":"masterOut","parent":"master"}},
+            
+            {"data":{"id":"main"}},
+            {"data":{"id":"oscillator","parent":"main"}},
+            {"data":{"id":"filter1","parent":"main"}},
+            {"data":{"id":"filter2","parent":"main"}},
+            {"data":{"id":"envelope","parent":"main"}},
+            {"data":{"id":"volume","parent":"main"}},
+    
+            {"data":{"id":"modulation1","parent":"main"}},
+            {"data":{"id":"mod1In","parent":"modulation1"}},
+            {"data":{"id":"mod1Scale","parent":"modulation1"}},
+            {"data":{"id":"mod1Ratio","parent":"modulation1"}},
+            {"data":{"id":"mod1Mul","parent":"modulation1"}},
+            {"data":{"id":"mod1Out","parent":"modulation1"}},
+    
+            {"data":{"id":"modulation2","parent":"main"},renderedPosition: { x: 200, y: 200 }},
+            {"data":{"id":"mod2In","parent":"modulation2"}},
+            {"data":{"id":"mod2Scale","parent":"modulation2"}},
+            {"data":{"id":"mod2Ratio","parent":"modulation2"}},
+            {"data":{"id":"mod2Mul","parent":"modulation2"}},
+            {"data":{"id":"mod2Out","parent":"modulation2"}},
+    
+            // Edges
+    
+            {"data":{"id":"w0","source":"oscillator","target":"filter1"}},
+            {"data":{"id":"w1","source":"oscillator","target":"filter2"}},
+            {"data":{"id":"w2","source":"filter1","target":"envelope"}},
+            {"data":{"id":"w3","source":"filter2","target":"envelope"}},
+            {"data":{"id":"w4","source":"envelope","target":"volume"}},
+    
+            {"data":{"id":"w5","source":"mod1In","target":"mod1Scale"}},
+            {"data":{"id":"w6","source":"mod2In","target":"mod2Scale"}},
+            {"data":{"id":"w7","source":"mod1Scale","target":"mod1Ratio"}},
+            {"data":{"id":"w8","source":"mod2Scale","target":"mod2Ratio"}},
+            {"data":{"id":"w9","source":"mod1Ratio","target":"mod1Mul"}},
+            {"data":{"id":"w10","source":"mod2Ratio","target":"mod2Mul"}},
+            {"data":{"id":"w11","source":"mod1Mul","target":"mod1Out"}},
+            {"data":{"id":"w12","source":"mod2Mul","target":"mod2Out"}},
+            
+            {"data":{"id":"w13","source":"masterComp","target":"masterReverb"}},
+            {"data":{"id":"w14","source":"masterReverb","target":"masterLimiter"}},
+            {"data":{"id":"w15","source":"masterLimiter","target":"masterOut"}},
+    
+            {"data":{"id":"w16","source":"volume","target":"masterComp"}},
+            {"data":{"id":"w17","source":"mod1Out","target":"filter1"}},
+            {"data":{"id":"w18","source":"mod1Out","target":"filter2"}},
+            {"data":{"id":"w19","source":"mod2Out","target":"envelope"}},
+            // {"data":{"id":"w20","source":"","target":""}},
+            
+            // {"data":{"id":"e3","source":"n0:n2","target":"n0:n5"}},
+            // {"data":{"id":"e4","source":"n2:n1","target":"n2:n4"}},
+            // {"data":{"id":"e5","source":"n0:n3","target":"n0:n4"}},
+            // {"data":{"id":"e6","source":"n3","target":"n2"}},
+            // {"data":{"id":"e7","source":"n5","target":"n6"}},
+            // {"data":{"id":"e8","source":"n5","target":"n2"}},
+            // {"data":{"id":"e9","source":"n3:n3","target":"n3:n2"}},
+            // {"data":{"id":"e11","source":"n0:n0","target":"n0:n1"}},
+            // {"data":{"id":"e12","source":"n0:n3","target":"n0:n2"}},
+            // {"data":{"id":"e13","source":"n3:n6","target":"n3:n0"}},
+            // {"data":{"id":"e14","source":"n3:n4","target":"n3:n2"}},
+            // {"data":{"id":"e15","source":"n3:n3","target":"n2:n0"}},
+            // {"data":{"id":"e16","source":"n2:n4","target":"n2:n7"}},
+            // {"data":{"id":"e17","source":"n3:n7","target":"n3:n3"}},
+            // {"data":{"id":"e18","source":"n3:n2","target":"n3:n0"}},
+            // {"data":{"id":"e19","source":"n6","target":"n4"}},
+            // {"data":{"id":"e20","source":"n4","target":"n3"}},
+            // {"data":{"id":"e23","source":"n0","target":"n7"}},
+            // {"data":{"id":"e24","source":"n0:n3","target":"n0:n5"}},
+            // {"data":{"id":"e28","source":"n0:n0","target":"n0:n2"}},
+            // {"data":{"id":"e30","source":"n0:n0","target":"n0:n3"}},
+            // {"data":{"id":"e33","source":"n7","target":"n3"}},
+            // {"data":{"id":"e34","source":"n3:n1","target":"n3:n7"}},
+            // {"data":{"id":"e36","source":"n0:n3","target":"n0:n1"}},
+            // {"data":{"id":"e39","source":"n3:n0","target":"n3:n4"}},
+            // {"data":{"id":"e41","source":"n2:n0","target":"n2:n4"}},
+            // {"data":{"id":"e43","source":"n3:n0","target":"n3:n5"}},
+            // {"data":{"id":"e45","source":"n7","target":"n4"}},
+            // {"data":{"id":"e46","source":"n2:n7","target":"n2:n0"}},
+            // {"data":{"id":"e47","source":"n2:n1","target":"n3:n6"}},
+            // {"data":{"id":"e49","source":"n3:n6","target":"n3:n5"}},
+            // {"data":{"id":"e1","source":"n3:n7","target":"n1:n4"}},
+            // {"data":{"id":"e2","source":"n0:n0","target":"n1:n4"}},
+            // {"data":{"id":"e10","source":"n1:n5","target":"n1:n4"}}
+        ]
         graph = cytoscape({
             container: divRef.current, // container to render in
-            elements: elements,        
+            elements: elements3,        
             style: [ // the stylesheet for the graph
                 {
                     selector: 'node',
@@ -4016,9 +4109,10 @@ const DiagramComponent = (props: any)=>{
                 }
             ],            
             layout: {
-            name: 'grid',
-            rows: 1
-            }
+                name: 'grid',
+                rows: 6,
+                cols: 5
+            }	
         }).on('cxttap', function(event) {
             if (allSelected('node')) {
               contextMenu.hideMenuItem('select-all-nodes');
@@ -4037,6 +4131,14 @@ const DiagramComponent = (props: any)=>{
               contextMenu.showMenuItem('select-all-edges');
             }
           });     
+        
+        /*         
+        * -----------------------
+        * | ANTD 레이아웃 구성 특성상 약간의 꿀렁임이 있다.            |
+        * | 이게 영 신경쓰이면 Grid로 Layout을 아예 구성 해버리면 된다 |
+        * | 시간이 녹록치 않아 일단 이렇게 해놓는다. |
+        * -----------------------
+        */
         graph.renderer().data.canvases.forEach((canvas: HTMLCanvasElement)=> {
             canvas.style.left = "0px";
             canvas.style.top = "0px";
@@ -4045,6 +4147,12 @@ const DiagramComponent = (props: any)=>{
         graph.on('resize', (event: any)=>{            
             graph.fit();
         })
+        
+        /*         
+        * -----------------------
+        * | context menu sample |
+        * -----------------------
+        */
         const allSelected = function (type: any) {
             if (type == 'node') {
               return graph.nodes().length == graph.nodes(':selected').length;
@@ -4053,23 +4161,23 @@ const DiagramComponent = (props: any)=>{
               return graph.edges().length == graph.edges(':selected').length;
             }
             return false;
-          }
+        }
   
-          const selectAllOfTheSameType = function (type: any) {
+        const selectAllOfTheSameType = function (type: any) {
             if (type == 'node') {
                 graph.nodes().select();
             } else if (type == 'edge') {
                 graph.edges().select();
             }
-          };
-          const unselectAllOfTheSameType = function (type: any) {
-            if (type == 'node') {
-                graph.nodes().unselect();
-              ;
-            } else if (type == 'edge') {
-                graph.edges().unselect();
-            }
-          };
+        };
+        const unselectAllOfTheSameType = function (type: any) {
+        if (type == 'node') {
+            graph.nodes().unselect();
+            ;
+        } else if (type == 'edge') {
+            graph.edges().unselect();
+        }
+        };
         const contextMenu = graph.contextMenus({
             menuItems: [
               {
@@ -4231,7 +4339,7 @@ const DiagramComponent = (props: any)=>{
                 }
               }
             ]
-          });
+        });
 
         return ()=> graph.destroy();
     }, []);
